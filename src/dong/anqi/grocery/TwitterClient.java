20f81c9b732ecdd3b58c8d7b3dfe7651ac66681f
Anqi Dong <anqid@google.com>
2020-04-12 20:56:35 -0700

    Make Twitter client a real usable client

---
+++ src/dong/anqi/grocery/TwitterClient.java
@@ -0,0 +1,45 @@
+package dong.anqi.grocery;
+
+import twitter4j.DirectMessage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterClient {
+  private static final TwitterFactory factory = createTwitterFactory();
+  private static TwitterFactory createTwitterFactory() {
+    Utils.Credentials accessToken = Utils.readCredentials("creds/twitter-access.creds");
+    Utils.Credentials apiKey = Utils.readCredentials("creds/twitter-consumer.creds");
+
+    Configuration conf = new ConfigurationBuilder()
+        .setDebugEnabled(true)
+        .setOAuthConsumerKey(apiKey.user)
+        .setOAuthConsumerSecret(apiKey.pass)
+        .setOAuthAccessToken(accessToken.user)
+        .setOAuthAccessTokenSecret(accessToken.pass)
+        .build();
+
+    return new TwitterFactory(conf);
+  }
+
+  private Twitter twitterInstance = factory.getInstance();
+  private static final String DEFAULT_USER = "SafeVarargs";
+  private static final long DEFAULT_USER_ID = 2783502499L;
+  public boolean sendDirectMessage(String message) {
+    try {
+      DirectMessage result = twitterInstance.sendDirectMessage(DEFAULT_USER, message);
+      System.out.println("sender " + result.getSenderId() + " recip " + result.getRecipientId());
+      return true;
+    } catch (TwitterException te) {
+      te.printStackTrace();
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    boolean successful = new TwitterClient().sendDirectMessage("test from refactored program");
+    System.exit(successful ? 0 : -1);
+  }
+}
